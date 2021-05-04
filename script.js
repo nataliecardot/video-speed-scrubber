54b8b8664c5983556a9305da94dee3a5ee2c5d9b
Natalie-Cardot <nataliecardot@gmail.com>
2021-05-04 07:50:51 -0700

    Change playback rate

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const bar = speed.querySelector('.speed-bar');
 const video = document.querySelector('.flex');
 
 // Using regular function so `this` is speed bar (element on which event triggered) - with an arrow function, since they use lexical scoping, `this` is inherited from parent scope (it would be Window)
-speed.addEventListener('mousemove', function (e) {
+function handleMove(e) {
   // pageY: y-coord of mouse event relative to entire document
   // offsetTop: distance between top position (px) of current element relative to offsetParent element (nearest positioned ancestor - if none, the nearest ancestor table element - if none, body)
   // getting coord within speed bar (it will be 0 at top)
@@ -11,11 +11,13 @@ speed.addEventListener('mousemove', function (e) {
   // offsetHeight: element's height including vertical padding and borders
   // So if mouse is hovered over middle of speed bar, percent will be 205 / 410 = 0.5
   const proportion = y / this.offsetHeight;
-  console.log(proportion);
   const min = 0.5;
-  const max = 3.5;
+  const max = 3.51;
   const height = Math.round(proportion * 100) + '%';
   const playbackRate = proportion * (max - min) + min;
   bar.style.height = height;
-  bar.textContent = playbackRate.toFixed(2);
-});
+  bar.textContent = playbackRate.toFixed(2) + '×';
+  video.playbackRate = playbackRate;
+}
+
+speed.addEventListener('mousemove', handleMove);
